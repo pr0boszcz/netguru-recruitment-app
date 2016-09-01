5f58ed3d2b734dde70464591289752b5ee1c1a98
proboszcz <coolerix@gmail.com>
2016-09-01 23:22:32 +0200

    update routes with root_path

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
+
+  root 'visitors#index'
+
   devise_for :users
+
   resources :students do
     get :subjects
   end
+
   resources :teachers do
     get :subjects
   end
+
   resources :visitors, only: [:index]
+
   get 'subjects' => 'reports#subjects', as: 'report_subjects'
 end
