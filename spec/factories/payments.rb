a2a8774816c08da735f2df86d36dab4166ed1c37
proboszcz <coolerix@gmail.com>
2016-09-03 19:14:35 +0200

    specs for payments

--- spec/factories/payments.rb
+++
@@ -1,9 +0,0 @@
-FactoryGirl.define do
-  factory :payment do
-    paid_at "2016-09-02"
-student nil
-month "MyString"
-year 1
-  end
-
-end
--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Payment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { is_expected.to validate_presence_of :year }
+    it { is_expected.to validate_presence_of :month }
+    it { is_expected.to validate_presence_of :paid_at }
+    it { is_expected.to validate_presence_of :student }
+  end
+
+  describe 'database columns' do
+    it { should have_db_column :year }
+    it { should have_db_column :month }
+    it { should have_db_column :paid_at }
+    it { should have_db_column :student_id }
+  end
+
+  describe 'associations' do
+    it { is_expected.to belong_to :student }
+  end
 end
