790656c9249ee6131b0b14f5a05e3ab8b997ed33
proboszcz <coolerix@gmail.com>
2016-09-03 13:27:13 +0200

    update controllers and localization for payments

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -4,14 +4,19 @@ class PaymentsController < ApplicationController
 
   def create
     if payment.save
-      redirect_to payment_path(payment), notice: I18n.t('shared.created', resource: 'Payment')
+      redirect_to payments_path, notice: I18n.t('shared.created', resource: 'Payment')
     else
       render :new
     end
   end
 
+  def destroy
+    payment.destroy
+    redirect_to payments_path, notice: I18n.t('shared.deleted', resource: 'Payment')
+  end
+
   private
   def payment_params
-    params.require(:payment).permit(:month, :year, :paid_at)
+    params.require(:payment).permit(:month, :year, :paid_at, :student_id)
   end
 end
