13cbaca13c16a237a72f9a3e99f18aea8540d54a
proboszcz <coolerix@gmail.com>
2016-09-03 20:17:54 +0200

    remove Fake from seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,17 @@
 puts "Seeds: start"
 TEACHER_TITLES = %w(Dr. Prof. TA)
 User.create!(email: 'admin@admin.com',password: 'adminadmin')
+LAST_NAMES = %w(Kowalski Nowak Bącki Jerzyna Zaporożec Stęchły Małolepszy Słuchawka Waligóra Zatyka)
+FIRST_NAMES = %w(Mieszko Borzygniew Przybowoj Wilkomir Budzigniew Wielebyt)
+SUBJECTS_PRE = %w(Historia Geografia Socjologia Zoografia Zoologia Fizyka)
+SUBJECTS_MID = %w(Ruchów Poczynań Ludów Stworzeń Społeczeństw Możliwości)
+SUBJECTS_POST = %w(Krótkowzrocznych Długouchych Małonosych Kolorowookich Ciemnopodbniebiennych)
+
 
 3.times do
   Teacher.create!(
-    first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name,
+    first_name: FIRST_NAMES.sample,
+    last_name: LAST_NAMES.sample,
     academic_title: TEACHER_TITLES.sample
   )
 end
@@ -13,15 +19,15 @@ end
 teachers = Teacher.all
 5.times do
   SubjectItem.create!(
-    title: Faker::Lorem.sentence,
+    title: "#{SUBJECTS_PRE.sample} #{SUBJECTS_MID.sample} #{SUBJECTS_POST.sample}",
     teacher: teachers.sample
   )
 end
 
 25.times do
   Student.create!(
-    first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name
+    first_name: FIRST_NAMES.sample,
+    last_name: LAST_NAMES.sample
   )
 end
 
